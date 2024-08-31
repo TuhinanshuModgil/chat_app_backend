d3a39198cd47b1cfcfb80c12460104fcfe5e37bf
Parisha Aggarwal <2021eeb1049@iitrpr.ac.in>
2024-08-31 15:23:33 +0530

    created institute model

---
+++ models/institute.model.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Define the schema for storing usernames and emails
+const InstituteSchema = new Schema({
+    instituteName: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    email: [{
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        
+    }],
+    users: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            default: [],
+        },
+    ],
+});
+
+// Compile model from schema
+const Institute = mongoose.model('Institute', InstituteSchema);
+
+module.exports = Institute;
